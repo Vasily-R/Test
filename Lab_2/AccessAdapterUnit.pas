843dbe7a315553c3c56829421803dfb29955218b
Vasily-R <mechenniy24@gmail.com>
2019-03-06 17:37:53 +0700

    Extract_Method_(AccessAdapter.getQuest)

--- Lab_2/AccessAdapterUnit.pas
+++ Lab_2/AccessAdapterUnit.pas
@@ -17,12 +17,16 @@ type
     function getAnswer: TList<string>;
     function getAnswerTableName: string;
     function getAnswerTable(answer: string): TList<string>;
-    //
     function getMenu: TList<string>;
     //
     procedure setTest(caption: string);
+    //
     function getQuest: TList<string>;
+    function getQuestTableName: string;
+    function getQuestTable(quest: string): TList<string>;
+    //
     function getCorrect: TDictionary<integer, integer>;
+
   published
     constructor create;
   end;
@@ -41,6 +45,30 @@ begin
       'Data Source=Phisics.accdb;' + 'persist security info=false';
     connected := true;
   end;
+end;
+
+function AccessAdapter.getQuestTable(quest: string): TList<string>;
+var
+  ADOQuery: TADOQuery;
+begin
+  result := TList<string>.create;
+  ADOQuery := TADOQuery.create(nil);
+  with (ADOQuery) do
+  begin
+    Connection := ADOConnection;
+    close;
+    sql.Clear;
+    sql.Add('SELECT caption FROM ' + quest + ';');
+    open;
+    Active := true;
+  end;
+  ADOQuery.First;
+  while not ADOQuery.Eof do
+  begin
+    result.Add(ADOQuery.FieldByName('caption').AsString);
+    ADOQuery.Next;
+  end;
+  ADOQuery.Free;
 end;
 
 function AccessAdapter.getAnswer: TList<string>;
@@ -62,7 +90,7 @@ begin
     end;
     ADOQuery.First; }
   answer := getAnswerTableName; { ADOQuery.FieldByName('answer').AsString; } //
-  //result := TList<string>.create;
+  // result := TList<string>.create;
   result := getAnswerTable(answer);
   { with (ADOQuery) do
     begin
@@ -125,7 +153,7 @@ begin
   result := ADOQuery.FieldByName('answer').AsString; //
 end;
 
-function AccessAdapter.getCorrect: TDictionary<integer, integer>;
+function AccessAdapter.getCorrect: TDictionary<integer, integer>; { }
 var
   ADOQuery: TADOQuery;
   correct: string;
@@ -161,7 +189,7 @@ begin
   ADOQuery.Free;
 end;
 
-function AccessAdapter.getMenu: TList<string>;
+function AccessAdapter.getMenu: TList<string>; { }
 var
   ADOQuery: TADOQuery;
   DataSource: TDataSource;
@@ -186,12 +214,51 @@ begin
   ADOQuery.Free;
 end;
 
-function AccessAdapter.getQuest: TList<string>;
+function AccessAdapter.getQuest: TList<string>; { }
 var
   ADOQuery: TADOQuery;
   quest: string;
+
 begin
-  result := TList<string>.create;
+  { result := TList<string>.create;
+    ADOQuery := TADOQuery.create(nil);
+    with (ADOQuery) do
+    begin
+    Connection := ADOConnection;
+    close;
+    sql.Clear;
+    sql.Add('SELECT quest FROM Main WHERE caption="' + self.caption + '";');
+    open;
+    Active := true;
+    end;
+    ADOQuery.First;
+    quest := ADOQuery.FieldByName('quest').AsString; }
+  quest := getQuestTableName; { ADOQuery.FieldByName('answer').AsString; } //
+  // result := TList<string>.create;
+  result := getQuestTable(quest);
+  { with (ADOQuery) do
+    begin
+    close;
+    sql.Clear;
+    sql.Add('SELECT caption FROM ' + quest + ';');
+    open;
+    Active := true;
+    end;
+    ADOQuery.First;
+    while not ADOQuery.Eof do
+    begin
+    result.Add(ADOQuery.FieldByName('caption').AsString);
+    ADOQuery.Next;
+    end;
+    ADOQuery.Free; }
+end;
+
+function AccessAdapter.getQuestTableName: string;
+var
+  ADOQuery: TADOQuery;
+  // quest: string;
+begin
+  // result := TList<string>.create;
   ADOQuery := TADOQuery.create(nil);
   with (ADOQuery) do
   begin
@@ -203,22 +270,8 @@ begin
     Active := true;
   end;
   ADOQuery.First;
-  quest := ADOQuery.FieldByName('quest').AsString;
-  with (ADOQuery) do
-  begin
-    close;
-    sql.Clear;
-    sql.Add('SELECT caption FROM ' + quest + ';');
-    open;
-    Active := true;
-  end;
-  ADOQuery.First;
-  while not ADOQuery.Eof do
-  begin
-    result.Add(ADOQuery.FieldByName('caption').AsString);
-    ADOQuery.Next;
-  end;
-  ADOQuery.Free;
+  { quest } result := ADOQuery.FieldByName('quest').AsString;
+
 end;
 
 procedure AccessAdapter.setTest(caption: string);
